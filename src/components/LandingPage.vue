d01808ade2faf6e3a81caae143ef71bf4f311de6
Viktor Malmedal <id17vml@cs.umu.se>
2020-05-26 14:42:03 +0200

    init

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class LandingPage extends Vue {
+  @Prop() private msg!: string;
+  hello = "hello world";
+}
+</script>
