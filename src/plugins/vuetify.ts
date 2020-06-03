24d748fb0c6168840b3d01b396f1008a24525e09
Viktor Malmedal <id17vml@cs.umu.se>
2020-06-03 23:25:50 +0200

    need to add the icon in some cool way

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import SettingsIcon from '@/components/SettingsIcon.vue';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        values: {
+            custom: {
+                component: SettingsIcon,
+            },
+        },
+    },
     breakpoint: {
         thresholds: {
             xs: 376,
